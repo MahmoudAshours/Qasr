87deadb14e0c003a004cb0925ea2e25f633660a3
Mahmoud Ashour <mashour365@gmail.com>
2025-06-20 22:50:33 +0300

    Refactor Link struct and LinkRepository methods for improved MongoDB integration

--- internal/repo/mongodb/link_repo.go
+++ internal/repo/mongodb/link_repo.go
@@ -1,16 +1,49 @@
 package mongodb
 
 import (
+	"context"
+	"errors"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Link struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Slug      string             `bson:"slug"`
-	TargetURL string             `bson:"target_url"`
-	Clicks    int                `bson:"clicks"`
-	CreatedAt time.Time          `bson:"created_at"`
-	ExpiresAt *time.Time         `bson:"expires_at,omitempty"`
+	Slug        string    `bson:"slug"`
+	OriginalURL string    `bson:"original_url"`
+	CreatedAt   time.Time `bson:"created_at"`
+}
+
+type LinkRepository struct {
+	Collection *mongo.Collection
+}
+
+func NewLinkRepository(db *mongo.Database) *LinkRepository {
+	return &LinkRepository{
+		Collection: db.Collection("links"),
+	}
+}
+
+func (r *LinkRepository) Create(slug, url string) error {
+	_, err := r.Collection.InsertOne(context.TODO(), Link{
+		Slug:        slug,
+		OriginalURL: url,
+		CreatedAt:   time.Now(),
+	})
+	return err
+}
+
+func (r *LinkRepository) FindBySlug(slug string) (string, error) {
+	var link Link
+	err := r.Collection.FindOne(context.TODO(), bson.M{"slug": slug}).Decode(&link)
+	if err != nil {
+		return "", errors.New("not found")
+	}
+	return link.OriginalURL, nil
+}
+
+func (r *LinkRepository) SlugExists(slug string) bool {
+	count, _ := r.Collection.CountDocuments(context.TODO(), bson.M{"slug": slug})
+	return count > 0
 }
