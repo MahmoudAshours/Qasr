8a4c43b7f8e7d88534ae1255237981968d55a14a
Mahmoud Ashour <mashour365@gmail.com>
2025-06-20 21:58:20 +0300

    Refactor main function to improve route definitions and server initialization

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	handler "qasr/internal/handler/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Define root route
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Qasr is live 👑")
+	})
+	r.POST("/shorten", handler.Shorten)
+	r.GET("/r/:slug", handler.Redirect)
+	// Start server
+	r.Run(":8080") // Default is localhost:8080
 }
