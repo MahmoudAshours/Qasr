423131d021f6ba903705c3449ed64c8f727a93c9
Mahmoud Ashour <mashour365@gmail.com>
2025-06-20 23:18:50 +0300

    Refactor ShortenerService to integrate Redis caching and improve slug management

--- internal/app/shortener/service.go
+++ internal/app/shortener/service.go
@@ -2,27 +2,51 @@ package shortener
 
 import (
 	"qasr/internal/repo/mongodb"
+	redisCache "qasr/internal/repo/redis"
 	"qasr/internal/utils"
+	"time"
 )
 
 type ShortenerService struct {
-	Repo *mongodb.LinkRepository
+	Repo  *mongodb.LinkRepository
+	Cache *redisCache.RedisCache
 }
 
-func NewShortenerService(repo *mongodb.LinkRepository) *ShortenerService {
-	return &ShortenerService{Repo: repo}
+func NewShortenerService(repo *mongodb.LinkRepository, cache *redisCache.RedisCache) *ShortenerService {
+	return &ShortenerService{Repo: repo, Cache: cache}
 }
 
 func (s *ShortenerService) CreateShortLink(url string) string {
 	for {
 		slug, _ := utils.GenerateSecureSlug(6)
-		if !s.Repo.SlugExists(slug) {
+		if !s.SlugExists(slug) {
 			s.Repo.Create(slug, url)
+			s.Cache.Set(slug, url, 30*time.Minute)
 			return slug
 		}
 	}
 }
 
 func (s *ShortenerService) GetOriginalURL(slug string) (string, error) {
-	return s.Repo.FindBySlug(slug)
+	// Try Redis first
+	url, err := s.Cache.Get(slug)
+	if err == nil && url != "" {
+		return url, nil
+	}
+
+	// Fallback to MongoDB
+	url, err = s.Repo.FindBySlug(slug)
+	if err == nil {
+		_ = s.Cache.Set(slug, url, 30*time.Minute)
+	}
+	return url, err
+}
+
+func (s *ShortenerService) SlugExists(slug string) bool {
+	// Step 1: Check Redis
+	url, err := s.Cache.Get(slug)
+	if err == nil && url != "" {
+		return true
+	}
+	return false
 }
