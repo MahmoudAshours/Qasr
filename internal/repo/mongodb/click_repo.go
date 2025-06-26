433d00cd3c99d93e1022813a37321d74a9d7c22b
Mahmoud Ashour <mashour365@gmail.com>
2025-06-26 18:31:50 +0300

    Refactor ClickRepo to improve structure and readability; add methods for saving and retrieving clicks

--- internal/repo/mongodb/click_repo.go
+++ internal/repo/mongodb/click_repo.go
@@ -1,1 +1,39 @@
 package mongodb
+
+import (
+	"context"
+	model "qasr/internal/domain"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ClickRepo struct {
+	Collection *mongo.Collection
+}
+
+// 🔧 Constructor
+func NewClickRepo(db *mongo.Database) *ClickRepo {
+	return &ClickRepo{
+		Collection: db.Collection("clicks"),
+	}
+}
+
+func (r *ClickRepo) SaveClick(click *model.Click) error {
+	_, err := r.Collection.InsertOne(context.TODO(), click)
+	return err
+}
+
+func (r *ClickRepo) GetClicksBySlug(slug string) ([]*model.Click, error) {
+	cursor, err := r.Collection.Find(context.TODO(), bson.M{"slug": slug})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.TODO())
+
+	var clicks []*model.Click
+	if err := cursor.All(context.TODO(), &clicks); err != nil {
+		return nil, err
+	}
+	return clicks, nil
+}
